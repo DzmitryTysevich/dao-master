9b6f8da97d443d3cf7162a1ff84bc3f80ead3868
DzmitryTysevich <dztysevich@gmail.com>
2021-05-10 01:03:03 +0300

    rebuild tasks with sql changes

--- src/main/java/com/epam/rd/autocode/dao/DepartmentDaoImpl.java
+++ src/main/java/com/epam/rd/autocode/dao/DepartmentDaoImpl.java
@@ -9,57 +9,109 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 public class DepartmentDaoImpl implements DepartmentDao {
 
-    private static final String SELECT_QUERY = "select * from DEPARTMENT";
+    private static final String FROM_DEPARTMENT_WHERE_ID = "select * from DEPARTMENT where ID = ";
+    private static final String FROM_DEPARTMENT = "select * from DEPARTMENT";
     private static final String ID = "ID";
     private static final String NAME = "NAME";
     private static final String LOCATION = "LOCATION";
-    private final Map<BigInteger, Department> departmentsByMap = new HashMap<>();
 
     @Override
     public Optional<Department> getById(BigInteger Id) {
-        putDepartmentsToMap();
-        return Optional.ofNullable(departmentsByMap.get(Id));
+        return Optional.ofNullable(getDepartment(FROM_DEPARTMENT_WHERE_ID + Id));
     }
 
     @Override
     public List<Department> getAll() {
-        putDepartmentsToMap();
-        return new ArrayList<>(departmentsByMap.values());
-    }
-
-    @Override
-    public Department save(Department department) {
-        departmentsByMap.put(department.getId(), department);
-        return departmentsByMap.get(department.getId());
-    }
-
-    @Override
-    public void delete(Department department) {
-        departmentsByMap.remove(department.getId());
-    }
-
-    private void putDepartmentsToMap() {
+        List<Department> departments = new ArrayList<>();
         try {
             Connection connection = ConnectionSource.instance().createConnection();
             Statement statement = connection.createStatement();
-            ResultSet rs = statement.executeQuery(SELECT_QUERY);
+            ResultSet rs = statement.executeQuery(FROM_DEPARTMENT);
             while (rs.next()) {
-                departmentsByMap.put(new BigInteger(String.valueOf(rs.getString(ID))),
-                        new Department(
-                                new BigInteger(String.valueOf(rs.getString(ID))),
-                                rs.getString(NAME),
-                                rs.getString(LOCATION))
+                departments.add(new Department(
+                        new BigInteger(String.valueOf(rs.getString(ID))),
+                        rs.getString(NAME),
+                        rs.getString(LOCATION))
                 );
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        return departments;
+    }
+
+    @Override
+    public Department save(Department department) {
+        try {
+            Connection connection = ConnectionSource.instance().createConnection();
+            Statement statement = connection.createStatement();
+            if (isNonExistId(department)) {
+                statement.executeUpdate(buildInsert(
+                        department.getId().toString(),
+                        department.getName(),
+                        department.getLocation())
+                );
+            } else {
+                statement.executeUpdate(buildUpdate(
+                        department.getId().toString(),
+                        department.getName(),
+                        department.getLocation())
+                );
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return getDepartment(FROM_DEPARTMENT_WHERE_ID + department.getId());
+    }
+
+    @Override
+    public void delete(Department department) {
+        try {
+            Connection connection = ConnectionSource.instance().createConnection();
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(buildDelete(department.getId().toString()));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Department getDepartment(String query) {
+        Department department = null;
+        try {
+            Connection connection = ConnectionSource.instance().createConnection();
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(query);
+            while (rs.next()) {
+                department = new Department(
+                        new BigInteger(String.valueOf(rs.getString(ID))),
+                        rs.getString(NAME),
+                        rs.getString(LOCATION)
+                );
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return department;
+    }
+
+    private boolean isNonExistId(Department department) {
+        return getAll().stream().noneMatch(dep -> dep.getId().equals(department.getId()));
+    }
+
+    public String buildInsert(String id, String name, String location) {
+        return "insert into DEPARTMENT (ID, NAME, LOCATION) values ('" + id + "', '" + name + "', '" + location + "')";
+    }
+
+    public String buildUpdate(String id, String name, String location) {
+        return "update DEPARTMENT set NAME = '" + name + "', LOCATION = '" + location + "' where ID = '" + id + "'";
+    }
+
+    public String buildDelete(String id) {
+        return "delete from DEPARTMENT where ID = '" + id + "'";
     }
 }
--- src/main/java/com/epam/rd/autocode/dao/EmployeeDaoImpl.java
+++ src/main/java/com/epam/rd/autocode/dao/EmployeeDaoImpl.java
@@ -13,15 +13,14 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class EmployeeDaoImpl implements EmployeeDao {
 
-    private static final String SELECT_QUERY = "select * from EMPLOYEE";
+    private static final String FROM_EMPLOYEE_WHERE_ID = "select * from EMPLOYEE where ID = ";
+    private static final String FROM_EMPLOYEE = "select * from EMPLOYEE";
     private static final String ID = "ID";
     private static final String FIRST_NAME = "FIRSTNAME";
     private static final String LAST_NAME = "LASTNAME";
@@ -31,56 +30,21 @@ public class EmployeeDaoImpl implements EmployeeDao {
     private static final String SALARY = "SALARY";
     private static final String MANAGER = "MANAGER";
     private static final String DEPARTMENT = "DEPARTMENT";
-    private final Map<BigInteger, Employee> employeesByMap = new HashMap<>();
 
     @Override
     public Optional<Employee> getById(BigInteger Id) {
-        putEmployeesToMap();
-        return Optional.ofNullable(employeesByMap.get(Id));
+        return Optional.ofNullable(getEmployee(FROM_EMPLOYEE_WHERE_ID + Id));
     }
 
     @Override
     public List<Employee> getAll() {
-        putEmployeesToMap();
-        return new ArrayList<>(employeesByMap.values());
-    }
-
-    @Override
-    public Employee save(Employee employee) {
-        employeesByMap.put(employee.getId(), employee);
-        return employeesByMap.get(employee.getId());
-    }
-
-    @Override
-    public void delete(Employee employee) {
-        employeesByMap.remove(employee.getId());
-    }
-
-    @Override
-    public List<Employee> getByDepartment(Department department) {
-        putEmployeesToMap();
-        return employeesByMap.values().stream()
-                .filter(employee -> employee.getDepartmentId().equals(department.getId()))
-                .collect(Collectors.toList());
-    }
-
-    @Override
-    public List<Employee> getByManager(Employee employee) {
-        putEmployeesToMap();
-        return employeesByMap.values().stream()
-                .filter(employeeByMap -> employeeByMap.getManagerId().equals(employee.getId()))
-                .collect(Collectors.toList());
-    }
-
-    private void putEmployeesToMap() {
+        List<Employee> employees = new ArrayList<>();
         try {
             Connection connection = ConnectionSource.instance().createConnection();
             Statement statement = connection.createStatement();
-            ResultSet rs = statement.executeQuery(SELECT_QUERY);
+            ResultSet rs = statement.executeQuery(FROM_EMPLOYEE);
             while (rs.next()) {
-                employeesByMap.put(
-                        new BigInteger(String.valueOf(rs.getString(ID))),
-                        new Employee(
+                employees.add(new Employee(
                                 new BigInteger(String.valueOf(rs.getString(ID))),
                                 new FullName(
                                         rs.getString(FIRST_NAME),
@@ -98,5 +62,109 @@ public class EmployeeDaoImpl implements EmployeeDao {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        return employees;
+    }
+
+    @Override
+    public Employee save(Employee employee) {
+        try {
+            Connection connection = ConnectionSource.instance().createConnection();
+            Statement statement = connection.createStatement();
+            if (isNonExistId(employee)) {
+                statement.executeUpdate(buildInsert(employee));
+            } else {
+                statement.executeUpdate(buildUpdate(employee));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return getEmployee(FROM_EMPLOYEE_WHERE_ID + employee.getId());
+    }
+
+    @Override
+    public void delete(Employee employee) {
+        try {
+            Connection connection = ConnectionSource.instance().createConnection();
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(buildDelete(employee.getId().toString()));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Employee> getByDepartment(Department department) {
+        return getAll().stream()
+                .filter(employee -> employee.getDepartmentId().equals(department.getId()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Employee> getByManager(Employee employee) {
+        return getAll().stream()
+                .filter(e -> e.getManagerId().equals(employee.getId()))
+                .collect(Collectors.toList());
+    }
+
+    private Employee getEmployee(String query) {
+        Employee employee = null;
+        try {
+            Connection connection = ConnectionSource.instance().createConnection();
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(query);
+            while (rs.next()) {
+                employee = new Employee(
+                        new BigInteger(String.valueOf(rs.getString(ID))),
+                        new FullName(
+                                rs.getString(FIRST_NAME),
+                                rs.getString(LAST_NAME),
+                                rs.getString(MIDDLE_NAME)
+                        ),
+                        Position.valueOf(rs.getString(POSITION)),
+                        rs.getDate(HIREDATE).toLocalDate(),
+                        new BigDecimal(rs.getInt(SALARY)),
+                        new BigInteger(String.valueOf(rs.getInt(MANAGER))),
+                        new BigInteger(String.valueOf(rs.getInt(DEPARTMENT)))
+                );
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return employee;
+    }
+
+    private boolean isNonExistId(Employee employee) {
+        return getAll().stream().noneMatch(dep -> dep.getId().equals(employee.getId()));
+    }
+
+    public String buildInsert(Employee employee) {
+        return "insert into EMPLOYEE (ID, FIRSTNAME, LASTNAME, MIDDLENAME, POSITION, HIREDATE, SALARY, MANAGER, DEPARTMENT) " +
+                "values ('" +
+                employee.getId() + "', '" +
+                employee.getFullName().getFirstName() + "', '" +
+                employee.getFullName().getLastName() + "', '" +
+                employee.getFullName().getMiddleName() + "', '" +
+                employee.getPosition() + "', '" +
+                employee.getHired() + "', '" +
+                employee.getSalary() + "', '" +
+                employee.getManagerId() + "', '" +
+                employee.getDepartmentId() + "')";
+    }
+
+    public String buildUpdate(Employee employee) {
+        return "update EMPLOYEE set " +
+                "FIRSTNAME = '" + employee.getFullName().getFirstName() + "', " +
+                "LASTNAME = '" + employee.getFullName().getLastName() + "', " +
+                "MIDDLENAME = '" + employee.getFullName().getLastName() + "', " +
+                "POSITION = '" + employee.getPosition() + "', " +
+                "HIREDATE = '" + employee.getHired() + "', " +
+                "SALARY = '" + employee.getSalary() + "', " +
+                "MANAGER = '" + employee.getManagerId() + "', " +
+                "DEPARTMENT = '" + employee.getDepartmentId() + "', " +
+                "' where ID = '" + employee.getId() + "'";
+    }
+
+    public String buildDelete(String id) {
+        return "delete from EMPLOYEE where ID = '" + id + "'";
     }
 }
